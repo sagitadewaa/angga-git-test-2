63e9f555df2f15abd5add31baebb089300306816
Angga Sagita <dewaa314@gmail.com>
2025-11-11 14:56:13 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World !</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat alias numquam fugit inventore error similique ab repellendus cum nisi sunt qui harum natus assumenda, dolor molestias nostrum? Fuga, mollitia laudantium.</p>
 </body>
 </html>
